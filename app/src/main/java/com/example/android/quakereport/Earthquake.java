d5a572ffd6bec3fb063af67f85a51745fa580c15
luispuchades <luispuchades@live.com>
2017-06-28 06:15:54 +0200

    UDACITY_ANDROID_QuakeReport
    UDACITY - ANDROID - NANODEGREE - Quake Report App

---
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -0,0 +1,73 @@
+package com.example.android.quakereport;
+
+/**
+ * Created by luisp on 18/06/2017.
+ */
+
+public class Earthquake {
+
+    /** Magnitude of the earthquake */
+    private double mMagnitude;
+
+    /** Location of the earthquake */
+    private String mLocation;
+
+    /** Time of the earthquake */
+    private long mTimeInMilliseconds;
+
+    /** Website URL of the earthquake */
+    private String mUrl;
+
+    /* Constructor creation   */
+    /**
+     * Constructs a new {@link Earthquake} object.
+     *
+     * @param magnitude is the magnitude (size) of the earthquake
+     * @param location is the location where the earthquake happened
+     * @param timeInMilliseconds is the time in milliseconds (from the Epoch) when the
+     *                           earthquake happened
+     * @param url is the website URL to find more details about the earthquake
+     */
+    public Earthquake(double magnitude, String location, long timeInMilliseconds, String url) {
+
+        mMagnitude = magnitude;
+        mLocation = location;
+        mTimeInMilliseconds = timeInMilliseconds;
+        mUrl = url;
+    }
+
+
+     /* Methods for Earthquake */
+    /* ******************* */
+
+    /**
+     *  Get the Earthquake magnitude
+     */
+
+    public double getMagnitude() {
+        return mMagnitude;
+    }
+
+    /**
+     *  Get the Earthquake Location
+     */
+    public String getLocation() {
+        return mLocation;
+    }
+
+    /*
+     * Get the Earthquake date
+     */
+    public long getTimeInMilliseconds() {
+        return mTimeInMilliseconds;
+    }
+
+    /**
+     * Returns the website URL to find more information about the earthquake.
+     */
+    public String getUrl() {
+        return mUrl;
+    }
+
+
+}
